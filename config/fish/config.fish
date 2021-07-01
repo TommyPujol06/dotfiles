549c10e9967e90e3c00fcb15d29b8829e254ff01
Tommy Pujol <git@itstommy.xyz>
2021-07-01 23:15:28 +0200

    Add git ssh command variable.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,3 +17,4 @@ set -x GTK2_RC_FILES "$HOME/.config/gtkrc-2.0"
 set -x GNUPGHOME "$HOME/.config/gnupg"
 set -x LESSHISTFILE /dev/null
 set -x GPG_TTY (tty)
+set -x GIT_SSH_COMMAND "ssh -F $HOME/.config/ssh/config -o IdentitiesOnly=yes"
